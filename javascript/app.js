31b7497596470f6669d5282901ae05105e401758
Nikhil-Akash <70918423+Nikhil-Akash@users.noreply.github.com>
2020-10-03 19:56:29 +0530

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -4,3 +4,22 @@ import {
 
 
 setTimeout(() => getMovies("popular", "en"), 5000)
+
+let $dropdown = document.querySelector(".dropdown")
+let $language = document.querySelector(".language")
+
+$dropdown.addEventListener("mouseover", () => {
+    $language.style.display = ($dropdown.classList[1] == "fa-caret-down") ? "block" : "none"
+    $dropdown.classList = ($dropdown.classList[1] == "fa-caret-down") ? "fa fa-caret-up dropdown" : "fa fa-caret-down dropdown"
+
+})
+
+
+// document.querySelector(".eng").addEventListener("click", () => 
+// document.querySelector(".telugu").addEventListener("click", () => 
+// document.querySelector(".tamil").addEventListener("click", () => 
+// document.querySelector(".kannada").addEventListener("click", () =>
+// document.querySelector(".malayam").addEventListener("click", () => 
+// document.querySelector(".home").addEventListener("click", () => 
+// document.querySelector(".popular").addEventListener("click", () => 
+// document.querySelector(".upcoming").addEventListener("click", () => 
